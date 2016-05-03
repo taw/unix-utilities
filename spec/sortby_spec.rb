929c0470a042bfb3571d63de6f735ad57a5a3d45
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-05-03 14:56:04 +0100

    Specs for sortby

---
+++ spec/sortby_spec.rb
@@ -0,0 +1,29 @@
+describe "sortby" do
+  let(:binary) { Pathname(__dir__)+"../bin/sortby" }
+
+  it "sortby abs" do
+    IO.popen("#{binary} '$_.to_i.abs'", "r+") do |fh|
+      fh.puts "12"
+      fh.puts "-91"
+      fh.puts "34"
+      fh.puts "-17"
+      fh.close_write
+      expect(fh.read).to eq(
+        "12\n"+
+        "-17\n"+
+        "34\n"+
+        "-91\n"
+      )
+    end
+  end
+
+  it "sortby abs" do
+    IO.popen("#{binary} '[$_.size, $_]'", "r+") do |fh|
+      fh.puts %W[Lorem ipsum dolor sit amet, consectetur adipisicing elit]
+      fh.close_write
+      expect(fh.read.tr("\n", " ")).to eq(
+        "sit elit Lorem amet, dolor ipsum adipisicing consectetur "
+      )
+    end
+  end
+end
