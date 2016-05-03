4ec1858c86d558a97e2e41f7ccee224d7d0f6dea
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-05-03 23:45:26 +0100

    Make untested files show as pending not as fails

--- spec/coverage_spec.rb
+++ spec/coverage_spec.rb
@@ -1,7 +1,9 @@
 describe "coverage" do
   (Pathname(__dir__) + "../bin").children.each do |path|
-    it "#{path.basename} has some tests" do
-      expect(Pathname(__dir__) + "#{path.basename}_spec.rb").to exist
+    if (Pathname(__dir__) + "#{path.basename}_spec.rb").exist?
+      # OK
+    else
+      pending "#{path.basename} has some tests"
     end
   end
 end
