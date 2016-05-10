51783434f3525775a9f8e45f7fb9889dc8c20229
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-05-10 13:03:56 +0100

    Spec for xrmdir

---
+++ spec/xrmdir_spec.rb
@@ -0,0 +1,43 @@
+describe "xrmdir" do
+  let(:binary) { Pathname(__dir__)+"../bin/xrmdir" }
+
+  it "removes empty directories, killing .DS_Store on the way" do
+    Dir.chtmpdir do |path|
+      FileUtils.mkdir_p "one"
+      FileUtils.mkdir_p "two"
+      FileUtils.mkdir_p "three/four"
+      FileUtils.mkdir_p "three/five"
+      FileUtils.touch "two/.DS_Store"
+      FileUtils.touch "three/five/.DS_Store"
+      system "#{binary} *"
+      expect( path.find.map{|x| x.relative_path_from(path).to_s} ).to eq([
+        ".",
+        "three",
+        "three/five",
+        "three/five/.DS_Store",
+        "three/four",
+      ])
+    end
+  end
+
+  it "recursively removes empty directories, killing .DS_Store on the way" do
+    Dir.chtmpdir do |path|
+      FileUtils.mkdir_p "one"
+      FileUtils.mkdir_p "two"
+      FileUtils.mkdir_p "three/four"
+      FileUtils.mkdir_p "three/five"
+      FileUtils.mkdir_p "six/seven"
+      FileUtils.mkdir_p "six/eight"
+      FileUtils.touch "two/.DS_Store"
+      FileUtils.touch "three/five/.DS_Store"
+      FileUtils.touch "six/eight/nine.txt"
+      system "#{binary} -r *"
+      expect( path.find.map{|x| x.relative_path_from(path).to_s} ).to eq([
+        ".",
+        "six",
+        "six/eight",
+        "six/eight/nine.txt",
+      ])
+    end
+  end
+end
