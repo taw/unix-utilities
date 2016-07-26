cbcb9c7e2a14b1a44a12ab346c8c5cfe9a178585
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-07-26 04:05:52 +0100

    Made lastfm_status spec timezone-independent

--- spec/lastfm_status_spec.rb
+++ spec/lastfm_status_spec.rb
@@ -4,7 +4,7 @@ describe "lastfm_status" do
   # Something old and abandoned or close enough
   it "real account" do
     expect(`#{binary} niphree`).to match(
-      /\Aniphree's last song was `Master of Tides' by `Lindsey Stirling` at 2015-05-13 05:45:00 \+0100\nIt was \d+d \d+h \d+m\d+s ago\n\z/
+      /\Aniphree's last song was `Master of Tides' by `Lindsey Stirling' at 2015-05-13 05:45:00 \+\d{4}\nIt was \d+d \d+h \d+m\d+s ago\n\z/
     )
   end
 
