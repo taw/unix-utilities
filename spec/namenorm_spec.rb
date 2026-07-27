2e2d022638a8bc192151e64a2d487ca3cb618221
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2026-07-27 11:58:08 +0100

    Fixed how namenorm deals with nested paths

--- spec/namenorm_spec.rb
+++ spec/namenorm_spec.rb
@@ -16,4 +16,30 @@ describe "namenorm" do
       ])
     end
   end
+
+  it "leaves directories leading to the file alone" do
+    MockUnix.new do |env|
+      FileUtils.mkdir_p "My Dir/Sub Dir"
+      FileUtils.touch "My Dir/Sub Dir/Some File.TXT"
+      system binary.to_s, "My Dir/Sub Dir/Some File.TXT"
+      expect(env).to have_content([
+        "My Dir",
+        "My Dir/Sub Dir",
+        "My Dir/Sub Dir/some_file.txt",
+      ])
+    end
+  end
+
+  it "normalizes directories passed to it directly" do
+    MockUnix.new do |env|
+      FileUtils.mkdir_p "Outer Dir/Inner Dir"
+      FileUtils.touch "Outer Dir/Inner Dir/Keep This.TXT"
+      system binary.to_s, "Outer Dir/Inner Dir"
+      expect(env).to have_content([
+        "Outer Dir",
+        "Outer Dir/inner_dir",
+        "Outer Dir/inner_dir/Keep This.TXT",
+      ])
+    end
+  end
 end
